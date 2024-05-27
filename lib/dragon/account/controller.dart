1d6221f803b7609b81a87bcc70a22faefbf1ac4c
kar <hi@kar.gs>
2024-05-27 12:05:54 +0200

    wip

--- lib/dragon/account/controller.dart
+++ lib/dragon/account/controller.dart
@@ -2,6 +2,7 @@ import 'package:common/account/account.dart' as acc;
 import 'package:common/dragon/account/account_id.dart';
 import 'package:common/util/di.dart';
 
+// TODO: drop this legacy sync class eventually
 class AccountController {
   late final _store = dep<acc.AccountStore>();
   late final _accountId = dep<AccountId>();
@@ -11,7 +12,10 @@ class AccountController {
     if (_started) return;
     _started = true;
 
-    // TODO: drop this legacy sync class eventually
+    if (_store.account != null) {
+      _accountId.now = _store.account!.id;
+    }
+
     _store.addOn(acc.accountChanged, (_) {
       _accountId.now = _store.account!.id;
     });
